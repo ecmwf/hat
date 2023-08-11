42c19d72042840648acac5d5f5224686faaaac57
Corentin Carton de Wiart <corentin.carton@ecmwf.int>
2023-08-11 11:48:30 +0100

    update setup for pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="hat",
-    version="0.4.0",
-    author="ECMWF",
-    description="ECMWF Hydrological Analysis Tools",
-    packages=find_packages(where="hat"),
-    package_dir={"": "hat"},
-    package_data={"hat": ["config_json/*.json"]},
-    include_package_data=True,
-    install_requires=[],
-    tests_require=[],
-    # command line tool name to function mapping
-    entry_points={
-        "console_scripts": [
-            "hat-extract-timeseries=tools.extract_simulation_timeseries_cli:main",
-            "hat-hydrostats=tools.hydrostats_cli:main",
-        ],
-    },
-)
+setup()
