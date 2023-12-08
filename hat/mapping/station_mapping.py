6ef9a726e3f1c4011ee51b2a2989b6ae626e2ae8
dadiyorto <dadiyorto@sunjul.com>
2023-12-08 02:46:03 +0100

    mapped out difference of station and grid location and their metadata area difference

--- hat/mapping/station_mapping.py
+++ hat/mapping/station_mapping.py
@@ -2,62 +2,73 @@
 
 import argparse
 import pandas as pd
-import numpy as np
+import geopandas as gpd
+from shapely.geometry import Point, LineString
 from netCDF4 import Dataset
+import numpy as np
+from numpy.ma import is_masked
+from geopy.distance import geodesic
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    """Calculate the distance in kilometers between two points."""
+    return geodesic((lat1, lon1), (lat2, lon2)).kilometers
 
 
-def get_grid_index(lat, lon, lat_array, lon_array):
-    # Find the index of the closest latitude and longitude in the arrays
-    lat_idx = np.abs(lat_array - lat).argmin()
-    lon_idx = np.abs(lon_array - lon).argmin()
-    return lat_idx, lon_idx
-
-def copy_metadata_from_source(src_dataset, dst_dataset):
-    # Copy global attributes
-    dst_dataset.setncatts(src_dataset.__dict__)
-
-    # Copy dimensions
-    for name, dimension in src_dataset.dimensions.items():
-        dst_dataset.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
-
-    # Copy coordinate variables (like 'lat', 'lon') and their attributes
-    for name, variable in src_dataset.variables.items():
-        if name in ['lat', 'lon']:
-            x = dst_dataset.createVariable(name, variable.datatype, variable.dimensions)
-            dst_dataset[name][:] = src_dataset[name][:]
-            dst_dataset[name].setncatts(src_dataset[name].__dict__)
-
-
-def main(netcdf_file, variable, csv_file, station_name_col, lat_col, lon_col):
-    # Read station CSV
+def main(netcdf_file, nc_variable, csv_file, station_name_col, lat_col, lon_col, csv_variable):
+    # Read station CSV and filter out invalid data
     stations = pd.read_csv(csv_file)
-    
+    stations = stations[stations[csv_variable] > 0]  # Filter out stations with csv_variable <= 0 or NaN
+
     # Load netCDF data
     dataset = Dataset(netcdf_file, 'r')
     latitudes = dataset.variables['lat'][:]
     longitudes = dataset.variables['lon'][:]
-    data_variable = dataset.variables[variable][:]
+    nc_data = dataset.variables[nc_variable][:]
 
-    # Initialize an empty 2D array for the new data
-    new_data_array = np.full(data_variable.shape, np.nan, dtype=data_variable.dtype)  # Fill with NaNs
+    data_list = []
 
-    # Process each station and populate the new data array
     for index, station in stations.iterrows():
         lat, lon = station[lat_col], station[lon_col]
         lat_idx = (np.abs(latitudes - lat)).argmin()
         lon_idx = (np.abs(longitudes - lon)).argmin()
 
-        # Extract data for the corresponding grid cell and store it in the new data array
-        new_data_array[lat_idx, lon_idx] = data_variable[lat_idx, lon_idx]
+        grid_data = nc_data[lat_idx, lon_idx]
+        if is_masked(grid_data):
+            grid_data = np.nan
+        else:
+            grid_data = float(grid_data)
 
-    # Create a new netCDF file to save the modified data
-    with Dataset("modified_data.nc", "w", format="NETCDF4") as new_dataset:
-        # Copy global attributes, dimensions, and coordinate variables
-        copy_metadata_from_source(dataset, new_dataset)
+        csv_data = station[csv_variable]
+        area_difference = (csv_data - grid_data)*1e-6
 
-        # Create the modified variable
-        new_var = new_dataset.createVariable(variable, data_variable.dtype, ('lat', 'lon'))
-        new_var[:] = new_data_array
+        station_data = {
+            'station_name': station[station_name_col],
+            'station_lat': lat,
+            'station_lon': lon,
+            'grid_lat': latitudes[lat_idx],
+            'grid_lon': longitudes[lon_idx],
+            'nc_variable': grid_data,
+            'csv_variable': csv_data,
+            'area_difference': area_difference,
+            'distance_km': calculate_distance(lat, lon, latitudes[lat_idx], longitudes[lon_idx])
+        }
+
+        data_list.append(station_data)
+
+    df = pd.DataFrame(data_list)
+
+    # Creating separate GeoDataFrames for grid points, stations, and lines with specific attributes
+    gdf_grid_point = gpd.GeoDataFrame(df[['nc_variable', 'grid_lat', 'grid_lon']], 
+                                      geometry=[Point(xy) for xy in zip(df.grid_lon, df.grid_lat)])
+    gdf_station_point = gpd.GeoDataFrame(df[['station_name', 'csv_variable', 'station_lat', 'station_lon']], 
+                                         geometry=[Point(xy) for xy in zip(df.station_lon, df.station_lat)])
+    gdf_line = gpd.GeoDataFrame(df[['area_difference', 'distance_km', 'station_lon', 'station_lat', 'grid_lon', 'grid_lat']], 
+                                geometry=[LineString([(row['station_lon'], row['station_lat']), 
+                                                      (row['grid_lon'], row['grid_lat'])]) for index, row in df.iterrows()])
+
+    gdf_grid_point.to_file("grid_point.geojson", driver="GeoJSON")
+    gdf_station_point.to_file("station.geojson", driver="GeoJSON")
+    gdf_line.to_file("station2grid_line.geojson", driver="GeoJSON")
 
     dataset.close()
 
@@ -65,11 +76,12 @@ def main(netcdf_file, variable, csv_file, station_name_col, lat_col, lon_col):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process netCDF and station data.')
     parser.add_argument('netcdf_file', type=str, help='Path to the netCDF file')
-    parser.add_argument('variable', type=str, help='Variable name in the netCDF file')
+    parser.add_argument('nc_variable', type=str, help='Variable name in the netCDF file')
     parser.add_argument('csv_file', type=str, help='Path to the CSV file containing station data')
     parser.add_argument('station_name_col', type=str, help='Column name for station names in the CSV file')
     parser.add_argument('lat_col', type=str, help='Column name for latitude in the CSV file')
     parser.add_argument('lon_col', type=str, help='Column name for longitude in the CSV file')
+    parser.add_argument('csv_variable', type=str, help='Column name for the CSV variable')
     args = parser.parse_args()
 
-    main(args.netcdf_file, args.variable, args.csv_file, args.station_name_col, args.lat_col, args.lon_col)
+    main(args.netcdf_file, args.nc_variable, args.csv_file, args.station_name_col, args.lat_col, args.lon_col, args.csv_variable)
--- hat/mapping/station_mapping.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 /home/dadiyorto/freelance/02_ort_ecmwf/dev/hat/hat/mapping/station_mapping.py "$@"
