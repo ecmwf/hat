473a73b22d754bd40013357d93ecde9f5129abee
Corentin Carton de Wiart <corentin.carton@ecmwf.int>
2024-01-10 15:25:59 +0000

    update station mapping

--- hat/mapping/station_mapping.py
+++ hat/mapping/station_mapping.py
@@ -6,10 +6,12 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 from shapely.geometry import Point, LineString, box
-from netCDF4 import Dataset
+import xarray as xr
 from numpy.ma import is_masked
 from geopy.distance import geodesic
 from shapely.wkt import loads
+
+import hat
 
 
 def get_grid_index(lat, lon, latitudes, longitudes):
@@ -66,26 +68,56 @@ def create_grid_polygon(lat, lon, cell_size):
     half_cell = cell_size / 2
     return box(lon - half_cell, lat - half_cell, lon + half_cell, lat + half_cell)
 
+
 def process_station_data(station, 
                          latitudes, longitudes, nc_data, 
-                         max_neighboring_cells, max_area_diff, 
+                         max_neighboring_cells, min_area_diff, max_area_diff, 
                          lat_col, lon_col, station_name_col, csv_variable, 
                          cell_size,
                          manual_lat_col, manual_lon_col, manual_area):
-    """Process data for a single station."""
+    """
+    Process data for a single station.
+
+    Parameters:
+        station (dict): A dictionary containing station data.
+        latitudes (numpy.ndarray): Array of latitude values for the grid cells.
+        longitudes (numpy.ndarray): Array of longitude values for the grid cells.
+        nc_data (numpy.ndarray): Array of grid cell data.
+        max_neighboring_cells (int): Maximum number of neighboring cells to consider for finding the best matching grid cell.
+        min_area_diff (float): Minimum area difference percentage to consider for finding the best matching grid cell.
+        max_area_diff (float): Maximum area difference percentage to consider for finding the best matching grid cell.
+        lat_col (str): Column name for latitude in the station data.
+        lon_col (str): Column name for longitude in the station data.
+        station_name_col (str): Column name for station name in the station data.
+        csv_variable (str): Column name for the variable in the station data.
+        cell_size (float): Size of each grid cell in kilometers.
+        manual_lat_col (str): Column name for manually mapped latitude in the station data.
+        manual_lon_col (str): Column name for manually mapped longitude in the station data.
+        manual_area (str): Column name for manually mapped area in the station data.
+
+    Returns:
+        dict: A dictionary containing processed data for the station, nearest grid cell, and best matching grid cell (if applicable).
+    """
     lat, lon = station[lat_col], station[lon_col]
     station_area = float(station[csv_variable]) if station[csv_variable] else np.nan
     
-    #manually mapped variable
-    manual_lat = station.get(manual_lat_col, np.nan)
-    manual_lon = station.get(manual_lon_col, np.nan)
-    manual_lat = float(manual_lat) if not pd.isna(manual_lat) else np.nan
-    manual_lon = float(manual_lon) if not pd.isna(manual_lon) else np.nan
-    manual_area = float(station[manual_area]) if station[manual_area] else np.nan
-    manual_area_diff = area_diff_percentage(manual_area, station_area)
-    manual_distance_km = calculate_distance(lat, lon, manual_lat, manual_lon)
+    # manually mapped variable
+    if manual_area is not None:
+        manual_lat = station.get(manual_lat_col, np.nan)
+        manual_lon = station.get(manual_lon_col, np.nan)
+        manual_lat = float(manual_lat) if not pd.isna(manual_lat) else np.nan
+        manual_lon = float(manual_lon) if not pd.isna(manual_lon) else np.nan
+        manual_area = float(station[manual_area]) if station[manual_area] else np.nan
+        manual_area_diff = area_diff_percentage(manual_area, station_area)
+        manual_distance_km = calculate_distance(lat, lon, manual_lat, manual_lon)
+    else:
+        manual_lat = np.nan
+        manual_lon = np.nan
+        manual_area = np.nan
+        manual_area_diff = np.nan
+        manual_distance_km = np.nan
 
-    # Original grid point
+    # Nearest grid cell
     lat_idx, lon_idx = get_grid_index(lat, lon, latitudes, longitudes)
     near_grid_area = nc_data[lat_idx, lon_idx]
     near_grid_area = float(near_grid_area) if not is_masked(near_grid_area) else np.nan
@@ -93,14 +125,22 @@ def process_station_data(station,
     near_grid_polygon = create_grid_polygon(latitudes[lat_idx], longitudes[lon_idx], cell_size)
     near_distance_km = calculate_distance(lat, lon, latitudes[lat_idx], longitudes[lon_idx])
 
-    # Best matching grid point
-    new_lat_idx, new_lon_idx = find_best_matching_grid(lat, lon, latitudes, longitudes, nc_data, station_area, max_neighboring_cells, max_area_diff)
-    new_grid_area = nc_data[new_lat_idx, new_lon_idx]
-    new_grid_area = float(new_grid_area) if not is_masked(new_grid_area) else np.nan
-    new_area_diff = area_diff_percentage(new_grid_area, station_area)
-    new_grid_polygon = create_grid_polygon(latitudes[new_lat_idx], longitudes[new_lon_idx], cell_size)
-    new_distance_km = calculate_distance(lat, lon, latitudes[new_lat_idx], longitudes[new_lon_idx])
-
+    # if the area difference is greater than the minimum, find the best matching grid cell otherwise use the nearest grid cell
+    if near_area_diff > min_area_diff:
+        # Best matching upstream area grid cell within the specified search radius
+        new_lat_idx, new_lon_idx = find_best_matching_grid(lat, lon, latitudes, longitudes, nc_data, station_area, max_neighboring_cells, max_area_diff)
+        new_grid_area = nc_data[new_lat_idx, new_lon_idx]
+        new_grid_area = float(new_grid_area) if not is_masked(new_grid_area) else np.nan
+        new_area_diff = area_diff_percentage(new_grid_area, station_area)
+        new_grid_polygon = create_grid_polygon(latitudes[new_lat_idx], longitudes[new_lon_idx], cell_size)
+        new_distance_km = calculate_distance(lat, lon, latitudes[new_lat_idx], longitudes[new_lon_idx])
+    else:
+        # Use the nearest grid cell as the best matching grid cell
+        new_lat_idx, new_lon_idx = lat_idx, lon_idx
+        new_grid_area = near_grid_area
+        new_area_diff = near_area_diff
+        new_grid_polygon = near_grid_polygon
+        new_distance_km = near_distance_km
 
     return {
         # Station data
@@ -133,47 +173,61 @@ def process_station_data(station,
         'manual_distance_km': manual_distance_km
         }
 
+
 def main(config):
-    netcdf_file = config["netcdf_file"]
-    nc_variable = config["nc_variable"]
+    # grid and upstream area file
+    upstream_area_file = config["upstream_area_file"]
+    nc_grid_size_arcmin = config["nc_grid_size_arcmin"]
+    # stations file
     csv_file = config["csv_file"]
+    csv_ups_col = config["csv_ups_col"]
     station_name_col = config["csv_station_name_col"]
     lat_col = config["csv_lat_col"]
     lon_col = config["csv_lon_col"]
-    csv_variable = config["csv_variable"]
+    stations_epsg = config.get("stations_epsg", "4326")
+    stations_filter = config["stations_filter"]
+
+    # mapping parameters
     max_neighboring_cells = config["max_neighboring_cells"]
     max_area_diff = config["max_area_diff"]
     min_area_diff =config["min_area_diff"]
-    nc_grid_size_arcmin = config["nc_grid_size_arcmin"]
-    manual_lat_col = config["manual_lat_col"]
-    manual_lon_col = config["manual_lon_col"]
-    manual_area = config["manual_area"]
 
+    # manually mapped stations - for testing
+    manual_lat_col = config.get("manual_lat_col")
+    manual_lon_col = config.get("manual_lon_col")
+    manual_area = config.get("manual_area")
 
     # Read station CSV and filter out invalid data
-    stations = pd.read_csv(csv_file)
-    stations = stations[stations[csv_variable] > 0]
+    stations_filter += f",{csv_ups_col}>0"
+    stations = hat.observations.read_station_metadata_file(
+        csv_file,
+        [lon_col, lat_col],
+        stations_epsg,
+        stations_filter,
+    )
 
     # Load netCDF data
-    dataset = Dataset(netcdf_file, 'r')
-    latitudes = dataset.variables['lat'][:]
-    longitudes = dataset.variables['lon'][:]
-    nc_data = (dataset.variables[nc_variable][:])*1e-6
+    dataset = xr.open_dataset(upstream_area_file)
+    nc_variable = hat.data.find_main_var(dataset, min_dim=2)
+    nc_data = dataset[nc_variable]*1e-6  # Convert from m^2 to km^2
+    latitudes = dataset['lat'].values
+    longitudes = dataset['lon'].values
 
      # Convert 1 arc minute to degrees for grid cell size
-    cell_size = nc_grid_size_arcmin/ 60
+    cell_size = nc_grid_size_arcmin/60
+    cell_size_check = abs(latitudes[0] - latitudes[1])
+    assert cell_size == cell_size_check, "Grid cell size does not match the specified arc minute size"
 
     # Process each station and collect data in a list
     data_list = []
     for index, station in stations.iterrows():
         station_data = process_station_data(station, 
                                             latitudes, longitudes, nc_data, 
-                                            max_neighboring_cells, max_area_diff, 
-                                            lat_col, lon_col, station_name_col, csv_variable, 
+                                            max_neighboring_cells, min_area_diff, max_area_diff, 
+                                            lat_col, lon_col, station_name_col, csv_ups_col, 
                                             cell_size, 
                                             manual_lat_col, manual_lon_col, manual_area)
-        if station_data['near_area_diff'] > min_area_diff:
-            data_list.append(station_data)
+        data_list.append(station_data)
     df = pd.DataFrame(data_list)
 
     df['near_grid_polygon'] = df.apply(lambda row: create_grid_polygon(row['near_grid_lat'], row['near_grid_lon'], cell_size), axis=1)
@@ -186,12 +240,18 @@ def main(config):
     # Drop the Shapely object columns that were replaced by wkts instead
     df = df.drop(columns=['near_grid_polygon', 'new_grid_polygon'])
 
+    # create line between station and near grid
+    df['line'] = df.apply(lambda row: LineString([(row['station_lon'], row['station_lat']), (row['near_grid_lon'], row['near_grid_lat'])]), axis=1)
+
+    # create line between station and new grid
+    df['line_new'] = df.apply(lambda row: LineString([(row['station_lon'], row['station_lat']), (row['new_grid_lon'], row['new_grid_lat'])]), axis=1)
+
     # Create GeoDataFrames
     gdf_station_point = gpd.GeoDataFrame(df, geometry=[Point(xy) for xy in zip(df['station_lon'], df['station_lat'])])
     gdf_near_grid_polygon = gpd.GeoDataFrame(df, geometry=df['near_grid_polygon_wkt'].apply(loads))
     gdf_new_grid_polygon = gpd.GeoDataFrame(df, geometry=df['new_grid_polygon_wkt'].apply(loads))
-    gdf_line = gpd.GeoDataFrame(df, geometry=[LineString([(row['station_lon'], row['station_lat']), (row['near_grid_lon'], row['near_grid_lat'])]) for index, row in df.iterrows()])
-    gdf_line_new = gpd.GeoDataFrame(df, geometry=[LineString([(row['station_lon'], row['station_lat']), (row['new_grid_lon'], row['new_grid_lat'])]) for index, row in df.iterrows()])
+    gdf_line = gpd.GeoDataFrame(df, geometry=df['line'])
+    gdf_line_new = gpd.GeoDataFrame(df, geometry=df['line_new'])
 
     # Save to GeoJSON
     gdf_station_point.to_file("station.geojson", driver="GeoJSON")
@@ -207,7 +267,7 @@ def main(config):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process netCDF and station data.')
+    parser = argparse.ArgumentParser(description='Station mapping tool: maps stations on provided grid.')
     parser.add_argument('config_file', type=str, help='Path to the JSON configuration file')
 
     args = parser.parse_args()
