5d7ee23b56deeb43de332f7187b5b2c0c46569cd
Corentin Carton De Wiart <corentin.carton@ecmwf.int>
2024-01-11 10:31:24 +0000

    update config

--- hat/mapping/station_mapping.py
+++ hat/mapping/station_mapping.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import os
 import argparse
 import json
 import geopandas as gpd
@@ -12,6 +12,7 @@ from geopy.distance import geodesic
 from shapely.wkt import loads
 
 import hat
+from hat.observations import read_station_metadata_file
 
 
 def get_grid_index(lat, lon, latitudes, longitudes):
@@ -60,7 +61,7 @@ def find_best_matching_grid(lat, lon, latitudes, longitudes, nc_data, csv_value,
             if abs(area_diff) < min_diff and abs(area_diff) <= max_area_diff:
                 min_diff = abs(area_diff)
                 best_match = (i, j)
-    
+
     return best_match
 
 def create_grid_polygon(lat, lon, cell_size):
@@ -69,10 +70,10 @@ def create_grid_polygon(lat, lon, cell_size):
     return box(lon - half_cell, lat - half_cell, lon + half_cell, lat + half_cell)
 
 
-def process_station_data(station, 
-                         latitudes, longitudes, nc_data, 
-                         max_neighboring_cells, min_area_diff, max_area_diff, 
-                         lat_col, lon_col, station_name_col, csv_variable, 
+def process_station_data(station,
+                         latitudes, longitudes, nc_data,
+                         max_neighboring_cells, min_area_diff, max_area_diff,
+                         lat_col, lon_col, station_name_col, csv_variable,
                          cell_size,
                          manual_lat_col, manual_lon_col, manual_area):
     """
@@ -98,15 +99,15 @@ def process_station_data(station,
     Returns:
         dict: A dictionary containing processed data for the station, nearest grid cell, and best matching grid cell (if applicable).
     """
-    lat, lon = station[lat_col], station[lon_col]
+    lat, lon = float(station[lat_col]), float(station[lon_col])
     station_area = float(station[csv_variable]) if station[csv_variable] else np.nan
-    
+
     # manually mapped variable
     if manual_area is not None:
         manual_lat = station.get(manual_lat_col, np.nan)
         manual_lon = station.get(manual_lon_col, np.nan)
-        manual_lat = float(manual_lat) if not pd.isna(manual_lat) else np.nan
-        manual_lon = float(manual_lon) if not pd.isna(manual_lon) else np.nan
+        manual_lat = float(manual_lat) if not pd.isna(manual_lat) and manual_lat != "" else np.nan
+        manual_lon = float(manual_lon) if not pd.isna(manual_lon) and manual_lon != "" else np.nan
         manual_area = float(station[manual_area]) if station[manual_area] else np.nan
         manual_area_diff = area_diff_percentage(manual_area, station_area)
         manual_distance_km = calculate_distance(lat, lon, manual_lat, manual_lon)
@@ -174,10 +175,22 @@ def process_station_data(station,
         }
 
 
-def main(config):
+def main():
+
+    parser = argparse.ArgumentParser(description='Station mapping tool: maps stations on provided grid.')
+    parser.add_argument('config_file', type=str, help='Path to the JSON configuration file')
+
+    args = parser.parse_args()
+
+    # Load configuration from the specified JSON file
+    with open(args.config_file, 'r') as file:
+        config = json.load(file)
+
+    out_dir = config["out_directory"]
+
     # grid and upstream area file
     upstream_area_file = config["upstream_area_file"]
-    nc_grid_size_arcmin = config["nc_grid_size_arcmin"]
+    
     # stations file
     csv_file = config["csv_file"]
     csv_ups_col = config["csv_ups_col"]
@@ -185,12 +198,12 @@ def main(config):
     lat_col = config["csv_lat_col"]
     lon_col = config["csv_lon_col"]
     stations_epsg = config.get("stations_epsg", "4326")
-    stations_filter = config["stations_filter"]
+    stations_filter = config.get("stations_filter", "")
 
     # mapping parameters
     max_neighboring_cells = config["max_neighboring_cells"]
     max_area_diff = config["max_area_diff"]
-    min_area_diff =config["min_area_diff"]
+    min_area_diff = config["min_area_diff"]
 
     # manually mapped stations - for testing
     manual_lat_col = config.get("manual_lat_col")
@@ -198,8 +211,8 @@ def main(config):
     manual_area = config.get("manual_area")
 
     # Read station CSV and filter out invalid data
-    stations_filter += f",{csv_ups_col}>0"
-    stations = hat.observations.read_station_metadata_file(
+    stations_filter += f",{csv_ups_col} > 0"
+    stations = read_station_metadata_file(
         csv_file,
         [lon_col, lat_col],
         stations_epsg,
@@ -212,20 +225,20 @@ def main(config):
     nc_data = dataset[nc_variable]*1e-6  # Convert from m^2 to km^2
     latitudes = dataset['lat'].values
     longitudes = dataset['lon'].values
+    print(dataset)
 
-     # Convert 1 arc minute to degrees for grid cell size
-    cell_size = nc_grid_size_arcmin/60
-    cell_size_check = abs(latitudes[0] - latitudes[1])
-    assert cell_size == cell_size_check, "Grid cell size does not match the specified arc minute size"
+    # extract cell size from coordinates
+    cell_size = abs(latitudes[0] - latitudes[1])
 
     # Process each station and collect data in a list
     data_list = []
     for index, station in stations.iterrows():
-        station_data = process_station_data(station, 
-                                            latitudes, longitudes, nc_data, 
+        print(index)
+        station_data = process_station_data(station,
+                                            latitudes, longitudes, nc_data,
                                             max_neighboring_cells, min_area_diff, max_area_diff, 
                                             lat_col, lon_col, station_name_col, csv_ups_col, 
-                                            cell_size, 
+                                            cell_size,
                                             manual_lat_col, manual_lon_col, manual_area)
         data_list.append(station_data)
     df = pd.DataFrame(data_list)
@@ -241,39 +254,30 @@ def main(config):
     df = df.drop(columns=['near_grid_polygon', 'new_grid_polygon'])
 
     # create line between station and near grid
-    df['line'] = df.apply(lambda row: LineString([(row['station_lon'], row['station_lat']), (row['near_grid_lon'], row['near_grid_lat'])]), axis=1)
+    lines = df.apply(lambda row: LineString([(row['station_lon'], row['station_lat']), (row['near_grid_lon'], row['near_grid_lat'])]), axis=1)
 
     # create line between station and new grid
-    df['line_new'] = df.apply(lambda row: LineString([(row['station_lon'], row['station_lat']), (row['new_grid_lon'], row['new_grid_lat'])]), axis=1)
+    new_lines = df.apply(lambda row: LineString([(row['station_lon'], row['station_lat']), (row['new_grid_lon'], row['new_grid_lat'])]), axis=1)
 
     # Create GeoDataFrames
     gdf_station_point = gpd.GeoDataFrame(df, geometry=[Point(xy) for xy in zip(df['station_lon'], df['station_lat'])])
     gdf_near_grid_polygon = gpd.GeoDataFrame(df, geometry=df['near_grid_polygon_wkt'].apply(loads))
     gdf_new_grid_polygon = gpd.GeoDataFrame(df, geometry=df['new_grid_polygon_wkt'].apply(loads))
-    gdf_line = gpd.GeoDataFrame(df, geometry=df['line'])
-    gdf_line_new = gpd.GeoDataFrame(df, geometry=df['line_new'])
+    gdf_line = gpd.GeoDataFrame(df, geometry=lines)
+    gdf_line_new = gpd.GeoDataFrame(df, geometry=new_lines)
 
     # Save to GeoJSON
-    gdf_station_point.to_file("station.geojson", driver="GeoJSON")
-    gdf_near_grid_polygon.to_file("near_grid.geojson", driver="GeoJSON")
-    gdf_new_grid_polygon.to_file("new_grid.geojson", driver="GeoJSON")
-    gdf_line.to_file("station2grid_line.geojson", driver="GeoJSON")
-    gdf_line_new.to_file("station2grid_new_line.geojson", driver="GeoJSON")
+    gdf_station_point.to_file(os.path.join(out_dir, "stations.geojson"), driver="GeoJSON")
+    gdf_near_grid_polygon.to_file(os.path.join(out_dir, "near_grid.geojson"), driver="GeoJSON")
+    gdf_new_grid_polygon.to_file(os.path.join(out_dir, "new_grid.geojson"), driver="GeoJSON")
+    gdf_line.to_file(os.path.join(out_dir, "stations2grid_line.geojson"), driver="GeoJSON")
+    gdf_line_new.to_file(os.path.join(out_dir, "stations2grid_new_line.geojson"), driver="GeoJSON")
 
     # save to csv
-    gdf_station_point.to_csv("station.csv")
+    gdf_station_point.to_csv(os.path.join(out_dir, "stations.csv"))
 
     dataset.close()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Station mapping tool: maps stations on provided grid.')
-    parser.add_argument('config_file', type=str, help='Path to the JSON configuration file')
-
-    args = parser.parse_args()
-
-    # Load configuration from the specified JSON file
-    with open(args.config_file, 'r') as file:
-        config = json.load(file)
-
-    main(config)
+    main()
