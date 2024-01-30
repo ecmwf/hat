cad87f43777065f6081ab7a3fe0cc68debf7ef7b
dadiyorto <dadiyorto@sunjul.com>
2024-01-30 15:27:12 +0100

    updated after isort

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from geopy.distance import geodesic
+
 from hat.mapping.station_mapping import (
     calculate_area_diff_percentage,
     calculate_distance,
