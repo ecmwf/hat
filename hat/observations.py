7e9ca38bcc60fe7c2c98c8ec37dd89c42982f1f5
Oisín Morrison <oisinmmm@gmail.com>
2024-09-11 09:48:39 +0000

    ensure add_geometry_column ends up with a geopandas df and not pandas df

--- hat/observations.py
+++ hat/observations.py
@@ -5,7 +5,7 @@ from hat.data import is_csv, read_csv_and_cache
 from hat.filters import filter_dataframe
 
 
-def add_geometry_column(gdf: gpd.GeoDataFrame, coord_names):
+def add_geometry_column(gdf: gpd.GeoDataFrame, coord_names, epsg):
     """add geometry column to stations geodataframe. station must have valid
     coordinates, i.e. not missing data and plot somewhere on Earth"""
 
@@ -21,27 +21,22 @@ def add_geometry_column(gdf: gpd.GeoDataFrame, coord_names):
     gdf = gdf.dropna(subset=["x", "y"])
 
     # Filter rows that do not plot on Earth (e.g. -9999)
-    gdf = gdf[
-        (gdf["x"] >= -180) & (gdf["x"] <= 180) & (gdf["y"] >= -90) & (gdf["y"] <= 90)
-    ]
+    gdf = gdf[(gdf["x"] >= -180) & (gdf["x"] <= 180) & (gdf["y"] >= -90) & (gdf["y"] <= 90)]
 
     # Create a geometry column
-    gdf["geometry"] = gpd.points_from_xy(gdf[x_coord_name], gdf[y_coord_name])
+    gdf = gpd.GeoDataFrame(gdf, geometry=gpd.points_from_xy(gdf["x"], gdf["y"]), crs=f"EPSG:{epsg}")
 
     return gdf
 
 
-def read_station_metadata_file(
-    fpath: str, coord_names: str, epsg: int, filters: str = None
-) -> gpd.GeoDataFrame:
+def read_station_metadata_file(fpath: str, coord_names: str, epsg: int, filters: str = None) -> gpd.GeoDataFrame:
     """read hydrological stations from file. will cache as pickle object
     because .csv file used by the team takes 12 seconds to load"""
 
     try:
         if is_csv(fpath):
             gdf = read_csv_and_cache(fpath)
-            gdf = add_geometry_column(gdf, coord_names)
-            gdf = gdf.set_crs(epsg=epsg)
+            gdf = add_geometry_column(gdf, coord_names, epsg)
         else:
             gdf = gpd.read_file(fpath)
     except Exception:
